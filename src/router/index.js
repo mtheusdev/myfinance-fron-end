fece7648ecedc35c7d1812d4d468754115394048
Matheus Henrique <08990349931@edu.udesc.br>
2021-03-29 04:52:07 -0300

    ate aula 392

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import authRoutes from '@/modules/auth/router'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    ...authRoutes,
+    { path: '', redirect: '/login' }
+  ]
+})
+
+export default router
